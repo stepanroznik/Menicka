7292d6226c23fc853b97d098eee83ee395cfd5ed
roznik-navisys <roznik@navisys.cz>
2022-05-16 14:53:55 +0200

    chore: lint fixes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -36,10 +36,10 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
+    name: 'HelloWorld',
+    props: {
+        msg: String,
+    },
 });
 </script>
 
